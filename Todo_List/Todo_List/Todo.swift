1d56eb86e086c0a1747009ca66d76bbf9180f889
Maxime D <103654561+mdonzelot@users.noreply.github.com>
2023-12-12 13:30:12 +0100

    Ajout Classe todo de base

--- Todo_List/Todo_List/Todo.swift
+++ Todo_List/Todo_List/Todo.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 class ToDo {
+    var nom : String
+    var description : String
     
+    init(nom:String, description:String) {
+        self.nom = nom
+        self.description = description
+    }
 }
